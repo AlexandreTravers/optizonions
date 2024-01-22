fabf372ba017e96744293ca5e7d243be23d86fc2
martinoux <martin.fleurant@orange.fr>
2024-01-22 10:06:19 +0100

    mmmmmmmmh

--- modeles/al_pacino/contrainte1.py
+++ modeles/al_pacino/contrainte1.py
@@ -1,10 +1,14 @@
 from pycsp3 import *
 
+name = VarArray(size = 5, dom=range(5))
 time = VarArray(size = 5, dom=range(5))
 day = VarArray(size = 5, dom=range(5))
 film = VarArray(size = 5, dom=range(5))
 
 #contraintes globales
+satisfy (
+    AllDifferent(name)
+)
 satisfy (
     AllDifferent(time)
 )
@@ -14,21 +18,20 @@ satisfy (
 satisfy (
     AllDifferent(film)
 )
-satisfy (
-    film[0] != 3
-)
+
 #contrainte : de tout les films sortie au 21ème siècle, aucuns n'a été choisi par jessica. Un a ouvert la semaine, un autre a fermé la semaine
 satisfy (
-    (0 != film[0]) & (0 != film[4])
+    (name[0] != film[0]) & (name[0] != film[4])
 )
 
 satisfy (
     ((film[0] == day[0]) & (film[4] == day[4])) | ((film[0] == day[4]) & (film[4] == day[0]))
 )
 
-#Affichage du résultat
+if solve() is SAT:
+    print("C'est SAT")
+else :
+    print("c'est pas SAT")
 
-if solve(sols=ALL) :
-    print("Number of solutions: ", n_solutions())
 
 

11e672ec71426fdc35ce2b91de39d1237278e9e8
Alexandre  Travers <102867623+AlexandreTravers@users.noreply.github.com>
2024-01-23 10:00:32 +0100

    Contrainte

--- modeles/al_pacinoPackage/contrainte1.py
+++ modeles/al_pacinoPackage/contrainte1.py
@@ -1,11 +1,16 @@
 from pycsp3 import *
 
-def al_pacinoCon(matrix):
+def al_pacinoCon(datas):
+    name = VarArray(size=5, dom=range(5))
     time = VarArray(size = 5, dom=range(5))
     day = VarArray(size = 5, dom=range(5))
     film = VarArray(size = 5, dom=range(5))
+    matrice = [name, time, day, film]
 
     #contraintes globales
+    satisfy(
+        AllDifferent(name)
+    )
     satisfy (
         AllDifferent(time)
     )
@@ -18,25 +23,27 @@ def al_pacinoCon(matrix):
 
     #contrainte : de tout les films sortie au 21ème siècle, aucuns n'a été choisi par jessica. Un a ouvert la semaine, un autre a fermé la semaine
     satisfy (
-        (0 != film[0]) & (0 != film[4])
+        (name[0] != film[0]) & (name[0] != film[4])
     )
 
     satisfy (
         ((film[0] == day[0]) & (film[4] == day[4])) | ((film[0] == day[4]) & (film[4] == day[0]))
     )
 
-    for row in range(len(matrix)):
-        for column in range(len(matrix[row])):
-            if(matrix[row][column] == -1):
-                satisfy(
-                    film[row] != column
-                )
-            elif(matrix[row][column] == 1):
-                satisfy(
-                    film[row] == column
-                )
+    for dataRow in range(len(datas)):
+        for dataColumn in range(len(datas[dataRow])):
+            for matrixRow in range(len(datas[dataRow][dataColumn])):
+                for matrixColumn in range(len(datas[dataRow][dataColumn][matrixRow])):
+                    if(datas[dataRow][dataColumn][matrixRow][matrixColumn] == -1):
+                        satisfy(
+                            matrice[dataRow][matrixRow] != matrice[dataColumn][matrixColumn]
+                        )
+                    elif(datas[dataRow][dataColumn][matrixRow][matrixColumn] == 1):
+                        satisfy(
+                            matrice[dataRow][matrixRow] == matrice[dataColumn][matrixColumn]
+                        )
 
-    if solve():
+    if solve() is SAT:
         return True
     else:
         return False

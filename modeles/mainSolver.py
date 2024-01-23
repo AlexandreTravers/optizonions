628600caed87dceeb5b0ef53a1b21c899a154263
martin <martin.fleurant@orange.fr>
2024-01-23 18:12:32 +0100

    calcul pour toute les contraintes

--- modeles/mainSolver.py
+++ modeles/mainSolver.py
@@ -1,4 +1,5 @@
-from al_pacinoPackage.contrainte1 import al_pacinoCon
+from al_pacinoPackage import contrainte1,contrainte2,contrainte3,contrainte4,contrainte5,contrainte6,contrainte7
+from pycsp3 import *
 
 """
 matrix = [[[-1, 1, 0],
@@ -62,6 +63,20 @@ def transposée (matrix) :
     return resultat
 
 datas = [matrix[0], matrix[1], matrix[2],transposée(matrix[5]), transposée(matrix[3]),transposée(matrix[4])]
-print(al_pacinoCon(datas))
+resultats= []
+resultats.append(contrainte1.al_pacinoCon(datas))
+clear()
+resultats.append(contrainte2.al_pacinoCon(datas))
+clear()
+resultats.append(contrainte3.al_pacinoCon(datas))
+clear()
+resultats.append(contrainte4.al_pacinoCon(datas))
+clear()
+resultats.append(contrainte5.al_pacinoCon(datas))
+clear()
+resultats.append(contrainte6.al_pacinoCon(datas))
+clear()
+resultats.append(contrainte7.al_pacinoCon(datas))
+print(resultats)
 
 #print(al_pacinoCon(matrix))

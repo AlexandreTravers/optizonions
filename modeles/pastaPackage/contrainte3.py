c70850573f5b8fdd093659d24b64ad37a73a384f
Alexandre  Travers <102867623+AlexandreTravers@users.noreply.github.com>
2024-01-29 11:50:07 +0100

    modèle3

---
+++ modeles/pastaPackage/contrainte3.py
@@ -0,0 +1,47 @@
+from pycsp3 import *
+
+def pastaCon(datas):
+    shirt = VarArray(size = 5, dom = range(5))
+    name = VarArray(size = 5, dom = range(5))
+    surname = VarArray(size = 5, dom = range(5))
+    pasta = VarArray(size = 5, dom = range(5))
+    wine = VarArray(size = 5, dom = range(5))
+    age = VarArray(size = 5, dom = range(5))
+    matrice = [shirt, name, surname, pasta, wine, age]
+
+    satisfy (
+        AllDifferent(shirt),
+        AllDifferent(name),
+        AllDifferent(surname),
+        AllDifferent(pasta),
+        AllDifferent(wine),
+        AllDifferent(age),
+    )
+
+    # La plus jeune est en 3ème position
+    satisfy(
+        age[0] == 2
+    )
+
+    count = 0
+    for i in range(5):
+        for j in range(i + 1, 6):
+            for matrixRow in range(len(datas[count])):
+                for matrixColumn in range(len(datas[count][matrixRow])):
+                    if datas[count][matrixRow][matrixColumn] == -1:
+                        satisfy(
+                            matrice[i][matrixRow] != matrice[j][matrixColumn]
+                        )
+                    if datas[count][matrixRow][matrixColumn] == 1:
+                        satisfy(
+                            matrice[i][matrixRow] == matrice[j][matrixColumn]
+                        )
+            count += 1
+
+    if solve() is SAT:
+        return True
+    else:
+        return False
+
+
+

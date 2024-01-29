04a7aad683f6e1cc930bac354e5cfe77cc33fb26
martinoux <martin.fleurant@orange.fr>
2024-01-29 09:06:28 +0100

    voilà

--- modeles/mainSolver.py
+++ modeles/mainSolver.py
@@ -1,5 +1,5 @@
 from al_pacinoPackage import contrainte1,contrainte2,contrainte3,contrainte4,contrainte5,contrainte6,contrainte7
-from computerPackage import contrainte1 as comc1, contrainte2 as comc2
+from computerPackage import contrainte1 as comc1, contrainte2 as comc2,contrainte3 as comc3, contrainte4 as comc4
 from pycsp3 import *
 
 """
@@ -63,19 +63,32 @@ def transposée (matrix) :
     resultat = list(zip(*matrix))
     return resultat
 
-datas = [matrix[0], matrix[1], matrix[2],transposée(matrix[5]), transposée(matrix[3]),transposée(matrix[4])]
-resultats= []
-resultats.append(contrainte1.al_pacinoCon(datas))
-clear()
-resultats.append(contrainte2.al_pacinoCon(datas))
-clear()
-resultats.append(contrainte3.al_pacinoCon(datas))
-clear()
-resultats.append(contrainte4.al_pacinoCon(datas))
-clear()
-resultats.append(contrainte5.al_pacinoCon(datas))
-clear()
-resultats.append(contrainte6.al_pacinoCon(datas))
-clear()
-resultats.append(contrainte7.al_pacinoCon(datas))
-print(resultats)
+def main(matrix, pb) :
+    datas = [matrix[0], matrix[1], matrix[2],transposée(matrix[5]), transposée(matrix[3]),transposée(matrix[4])]
+    resultats= []
+    if pb == 1:
+        resultats.append(contrainte1.al_pacinoCon(datas))
+        clear()
+        resultats.append(contrainte2.al_pacinoCon(datas))
+        clear()
+        resultats.append(contrainte3.al_pacinoCon(datas))
+        clear()
+        resultats.append(contrainte4.al_pacinoCon(datas))
+        clear()
+        resultats.append(contrainte5.al_pacinoCon(datas))
+        clear()
+        resultats.append(contrainte6.al_pacinoCon(datas))
+        clear()
+        resultats.append(contrainte7.al_pacinoCon(datas))
+    elif pb == 2:
+        resultats.append(comc1(datas))
+        clear()
+        resultats.append(comc2(datas))
+        clear()
+        resultats.append(comc3(datas))
+        clear()
+        resultats.append(comc4(datas))
+        clear()
+    print(resultats)
+    return resultats
+

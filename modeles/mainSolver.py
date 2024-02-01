d46ccc0a3df0cdfbac4d2122eeb9835db6fe7a19
martin <martin.fleurant@orange.fr>
2024-02-01 14:38:15 +0100

    test

--- modeles/mainSolver.py
+++ modeles/mainSolver.py
@@ -2,116 +2,6 @@ from al_pacinoPackage import contrainte1 as pacc1,contrainte2 as pacc2,contraint
 from computerPackage import contrainte1 as comc1, contrainte2 as comc2,contrainte3 as comc3, contrainte4 as comc4
 from pastaPackage import contrainte1, contrainte2, contrainte3, contrainte4, contrainte5, contrainte6, contrainte7, contrainte8, contrainte9, contrainte10, contrainte11, contrainte12, contrainte13, contrainte14, contrainte15, contrainte16, contrainte17, contrainte18, contrainte19, contrainte20, contrainte21, contrainte22
 from pycsp3 import *
-
-matrix = [
-    [
-        [-1, -1, 1, -1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, 1]
-    ],
-    [
-        [-1, -1, 1, -1, -1],
-        [-1, -1, -1, -1, 1],
-        [-1, 1, -1, -1, -1],
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1]
-    ],
-    [
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, 1, -1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, -1, 1]
-    ],
-    [
-        [-1, -1, -1, 1, -1],
-        [-1, -1, -1, -1, 1],
-        [1, -1, -1, -1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, 1, -1, -1]
-    ],
-    [
-        [-1, -1, 1, -1, -1],
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, -1, 1]
-    ],
-    [
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, -1, 1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, 1, -1, -1]
-    ],
-    [
-        [-1, -1, -1, 1, -1],
-        [-1, -1, 1, -1, -1],
-        [1, -1, -1, -1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, -1, 1]
-    ],
-    [
-        [-1, -1, 1, -1, -1],
-        [-1, -1, -1, -1, 1],
-        [-1, -1, -1, 1, -1],
-        [-1, 1, -1, -1, -1],
-        [1, -1, -1, -1, -1]
-    ],
-    [
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, 1, -1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, -1, 1]
-    ],
-    [
-        [-1, -1, -1, 1, -1],
-        [-1, -1, -1, -1, 1],
-        [1, -1, -1, -1, -1],
-        [-1, -1, 1, -1, -1],
-        [-1, 1, -1, -1, -1]
-    ],
-    [
-        [-1, -1, -1, -1, 1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, 1, -1, -1],
-        [1, -1, -1, -1, -1]
-    ],
-    [
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, -1, -1, 1],
-        [-1, -1, 1, -1, -1],
-        [-1, 1, -1, -1, -1]
-    ],
-    [
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, -1, 1],
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, 1, -1, -1]
-    ],
-    [
-        [-1, -1, 1, -1, -1],
-        [1, -1, -1, -1, -1],
-        [-1, 1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, -1, -1, -1, 1]
-    ],
-    [
-        [-1, -1, -1, -1, 1],
-        [-1, -1, 1, -1, -1],
-        [1, -1, -1, -1, -1],
-        [-1, -1, -1, 1, -1],
-        [-1, 1, -1, -1, -1]
-    ]
-]
-
-
 """
 matrix = [[[-1, 1, -1, -1, -1],
           [-1, -1, -1, 1, -1],
@@ -148,88 +38,151 @@ matrix = [[[-1, 1, -1, -1, -1],
           [-1, 1, -1, -1, -1],
           [-1, -1, -1, 1, -1],
           [-1, -1, -1, -1, 1]]]
+
 """
-def transposée (matrix) :
-    resultat = list(zip(*matrix))
-    return resultat
+class ObjetMatrice:
+    def __init__(self, matrice, pb):
+        self.matrice = matrice
+        self.pb = pb
+        self.result = []
 
-def main(matrix, pb) :
-    if(pb == 3):
-        datas = []
-        for i in matrix:
-            datas.append(transposée(i))
-    else:
-        datas = [matrix[0], matrix[1], matrix[2], transposée(matrix[5]), transposée(matrix[3]), transposée(matrix[4])]
-    resultats= []
-    if pb == 1:
-        resultats.append(pacc1.al_pacinoCon(datas))
-        clear()
-        resultats.append(pacc2.al_pacinoCon(datas))
-        clear()
-        resultats.append(pacc3.al_pacinoCon(datas))
-        clear()
-        resultats.append(pacc4.al_pacinoCon(datas))
-        clear()
-        resultats.append(pacc5.al_pacinoCon(datas))
-        clear()
-        resultats.append(pacc6.al_pacinoCon(datas))
-        clear()
-        resultats.append(pacc7.al_pacinoCon(datas))
-    elif pb == 2:
-        resultats.append(comc1.computerCon(datas))
-        clear()
-        resultats.append(comc2.computerCon(datas))
-        clear()
-        resultats.append(comc3.computerCon(datas))
-        clear()
-        resultats.append(comc4.computerCon(datas))
-        clear()
-    elif pb == 3:
-        resultats.append(contrainte1.pastaCon(datas))
-        clear()
-        resultats.append(contrainte2.pastaCon(datas))
-        clear()
-        resultats.append(contrainte3.pastaCon(datas))
-        clear()
-        resultats.append(contrainte4.pastaCon(datas))
-        clear()
-        resultats.append(contrainte5.pastaCon(datas))
-        clear()
-        resultats.append(contrainte6.pastaCon(datas))
-        clear()
-        resultats.append(contrainte7.pastaCon(datas))
-        clear()
-        resultats.append(contrainte8.pastaCon(datas))
-        clear()
-        resultats.append(contrainte9.pastaCon(datas))
-        clear()
-        resultats.append(contrainte10.pastaCon(datas))
-        clear()
-        resultats.append(contrainte11.pastaCon(datas))
-        clear()
-        resultats.append(contrainte12.pastaCon(datas))
-        clear()
-        resultats.append(contrainte13.pastaCon(datas))
-        clear()
-        resultats.append(contrainte14.pastaCon(datas))
-        clear()
-        resultats.append(contrainte15.pastaCon(datas))
-        clear()
-        resultats.append(contrainte16.pastaCon(datas))
-        clear()
-        resultats.append(contrainte17.pastaCon(datas))
-        clear()
-        resultats.append(contrainte18.pastaCon(datas))
-        clear()
-        resultats.append(contrainte19.pastaCon(datas))
-        clear()
-        resultats.append(contrainte20.pastaCon(datas))
-        clear()
-        resultats.append(contrainte21.pastaCon(datas))
-        clear()
-        resultats.append(contrainte22.pastaCon(datas))
-        clear()
-    print(resultats)
-    return resultats
+    def transposée (self,matrix) :
+        resultat = list(zip(*matrix))
+        return resultat
+    
+    def setMatrix(self, matrice) :
+        self.matrice = matrice
 
-print(main(matrix, 3))
+    def setPb(self, pb): 
+        self.pb = pb
+    
+    def getMatrix(self):
+        return self.matrice
+
+    def getPb(self):
+        return self.pb
+    
+    def getResult(self) :
+        return self.result
+    
+    def main(self) :
+        if(self.pb == 3):
+            datas = []
+            for i in self.matrice:
+                datas.append(self.transposée(i))
+        else:
+            datas = [self.matrice[0], self.matrice[1], self.matrice[2], self.transposée(self.matrice[5]), self.transposée(self.matrice[3]), self.transposée(self.matrice[4])]
+        resultats= []
+        if self.pb == 1:
+            resultats.append(pacc1.al_pacinoCon(datas))
+            clear()
+            resultats.append(pacc2.al_pacinoCon(datas))
+            clear()
+            resultats.append(pacc3.al_pacinoCon(datas))
+            clear()
+            resultats.append(pacc4.al_pacinoCon(datas))
+            clear()
+            resultats.append(pacc5.al_pacinoCon(datas))
+            clear()
+            resultats.append(pacc6.al_pacinoCon(datas))
+            clear()
+            resultats.append(pacc7.al_pacinoCon(datas))
+        elif self.pb == 2:
+            resultats.append(comc1.computerCon(datas))
+            clear()
+            resultats.append(comc2.computerCon(datas))
+            clear()
+            resultats.append(comc3.computerCon(datas))
+            clear()
+            resultats.append(comc4.computerCon(datas))
+            clear()
+        elif self.pb == 3:
+            resultats.append(contrainte1.pastaCon(datas))
+            clear()
+            resultats.append(contrainte2.pastaCon(datas))
+            clear()
+            resultats.append(contrainte3.pastaCon(datas))
+            clear()
+            resultats.append(contrainte4.pastaCon(datas))
+            clear()
+            resultats.append(contrainte5.pastaCon(datas))
+            clear()
+            resultats.append(contrainte6.pastaCon(datas))
+            clear()
+            resultats.append(contrainte7.pastaCon(datas))
+            clear()
+            resultats.append(contrainte8.pastaCon(datas))
+            clear()
+            resultats.append(contrainte9.pastaCon(datas))
+            clear()
+            resultats.append(contrainte10.pastaCon(datas))
+            clear()
+            resultats.append(contrainte11.pastaCon(datas))
+            clear()
+            resultats.append(contrainte12.pastaCon(datas))
+            clear()
+            resultats.append(contrainte13.pastaCon(datas))
+            clear()
+            resultats.append(contrainte14.pastaCon(datas))
+            clear()
+            resultats.append(contrainte15.pastaCon(datas))
+            clear()
+            resultats.append(contrainte16.pastaCon(datas))
+            clear()
+            resultats.append(contrainte17.pastaCon(datas))
+            clear()
+            resultats.append(contrainte18.pastaCon(datas))
+            clear()
+            resultats.append(contrainte19.pastaCon(datas))
+            clear()
+            resultats.append(contrainte20.pastaCon(datas))
+            clear()
+            resultats.append(contrainte21.pastaCon(datas))
+            clear()
+            resultats.append(contrainte22.pastaCon(datas))
+            clear()
+        print(resultats)
+        self.result = resultats
+
+objet = ObjetMatrice([],[])
+matrice_exemple = [[[-1, 1, -1, -1, -1],
+          [-1, -1, -1, 1, -1],
+          [-1, -1, 1, -1, -1],
+          [-1, -1, -1, -1, 1],
+          [1, -1, -1, -1, -1]],
+
+          [[-1, -1, 1, -1, -1],
+          [-1, -1, -1, 1, -1],
+          [-1, 1, -1, -1, -1],
+          [-1, -1, -1, -1, 1],
+          [1, -1, -1, -1, -1]],
+
+          [[-1, -1, -1, 1, -1],
+          [-1, 1, -1, -1, -1],
+          [-1, -1, -1, -1, 1],
+          [1, -1, -1, -1, -1],
+          [-1, -1, 1, -1, -1]],
+
+          [[-1, -1, -1, -1, 1],
+          [-1, -1, -1, 1, -1],
+          [1, -1, -1, -1, -1],
+          [-1, 1, -1, -1, -1],
+          [-1, -1, 1, -1, -1]],
+
+          [[-1, -1, -1, -1, 1],
+          [-1, -1, -1, 1, -1],
+          [1, -1, -1, -1, -1],
+          [-1, -1, 1, -1, -1],
+          [-1, 1, -1, -1, -1]],
+
+          [[1, -1, -1, -1, -1],
+          [-1, -1, 1, -1, -1],
+          [-1, 1, -1, -1, -1],
+          [-1, -1, -1, 1, -1],
+          [-1, -1, -1, -1, 1]]]
+
+pb_exemple = 2
+objet.setMatrix(matrice_exemple)
+objet.setPb(pb_exemple)
+objet.main()
+print(objet.getResult)
